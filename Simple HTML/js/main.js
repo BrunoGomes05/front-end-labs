11ca2d9d826f5fb865430050abdb9929124f9980
BrunoGomes05 <brunorobim148@gmail.com>
2025-05-21 19:12:10 -0300

    introducao_dom

--- Simple HTML/js/main.js
+++ Simple HTML/js/main.js
@@ -1,2 +1,9 @@
-let nome = "Bruno"
-alert("Boa noite, " + nome); 
+const linkExterno = document.querySelector('a[title="Google Maps"]');
+ 
+//linkExterno.onclick = () => {
+    //alert("Você será redirecionado para um link externo.");
+//};
+ 
+linkExterno.addEventListener('click', () => {
+    alert("Você será redirecionado para um link externo.");
+});
