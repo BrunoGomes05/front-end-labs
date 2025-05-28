9daac97d3fb8055193c59b7a3030cf06dc3cd196
BrunoGomes05 <brunorobim148@gmail.com>
2025-05-28 19:04:03 -0300

    virtual phone final

--- virtual_phone/main.js
+++ virtual_phone/main.js
@@ -1,25 +1,35 @@
-const display = document.querySelector ('input[type=tel]');
+const display = 
+    document.querySelector('input[type=tel]');
  
-const teclas = document.querySelectorAll ('input[type=button]');
+const teclas = 
+    document.querySelectorAll('input[type=button]');
  
 const limpar = document.querySelector('.icon');
  
 limpar.onclick = () => {
-    const opcao = confirm('Deseja mesmo limpar?')
-    if(opcao) {
+    const opcao = confirm('Deseja mesmo limpar?');
+    if (opcao) {
         display.value = "";
-    }
-   
+    }    
 }
  
+//Tarefinha: Criar uma condição para travar
+//mais de 14 digitos
+
 function valorTeclaToDisplay (i) {
-    display.value += teclas[i].value;
+    if (display.value.length < 14) {
+        display.value += teclas[i].value;
+    }
 }
  
-//Criar uma condição para travar mais 14 digitos
+//Exemplo 
+//const limiteNumeros = 14;
+
+
+
+ 
 for(let i = 0; i < teclas.length; i++){
     teclas[i].onclick = () => {
         valorTeclaToDisplay(i);
     }
 }
- 
